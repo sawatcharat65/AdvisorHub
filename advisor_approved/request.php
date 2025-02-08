0e47cbeb178c5cce53ce000b60117d45446cad53
Anadyst <potter17180@gmail.com>
2025-02-08 18:27:44 +0700

    change ui advisor_approved

--- advisor_approved/request.php
+++ advisor_approved/request.php
@@ -20,22 +20,18 @@
 
 ?>
 
-<!DOCTYPE html>
 <html lang="th">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>อนุมัติคำร้อง</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="style_request.css">
-    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <link rel="icon" href="../Logo.png">
+
 </head>
 <body>
-
     <?php renderNavbar(['home', 'advisor', 'inbox', 'statistics', 'Teams'])?>
-    
+
     <div class="container">
         <?php
         date_default_timezone_set("Asia/Bangkok");
@@ -46,26 +42,24 @@
             ["title" => "ระบบจัดเก็บข้อมูลออนไลน์อัจฉริยะ", "eng_title" => "Intelligent Online Data Storage System", "students" => [["name" => "cccc ccc", "id" => "112233445", "major" => "CS"]]],
             ["title" => "การวิเคราะห์ข้อมูลขนาดใหญ่", "eng_title" => "Big Data Analytics", "students" => [["name" => "dddd ddd", "id" => "556677889", "major" => "IT"]]]
         ];
-
+        
         foreach ($thesis_list as $index => $thesis): 
             $timestamp = date("d-m-Y h:i:s"); ?>
             <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title text-primary">หัวข้อวิทยานิพนธ์: <?php echo $thesis["title"]; ?></h5>
-                    <h6 class="card-subtitle text-muted">(<?php echo $thesis["eng_title"]; ?>)</h6>
-                    <ul class="list-group list-group-flush">
-                        <?php foreach ($thesis["students"] as $student): ?>
-                            <li class="list-group-item">
-                                <strong>ชื่อ:</strong> <?php echo $student["name"]; ?> &nbsp;
-                                <strong>รหัสนิสิต:</strong> <?php echo $student["id"]; ?> &nbsp;
-                                <strong>สาขาวิชา:</strong> <?php echo $student["major"]; ?>
-                            </li>
-                        <?php endforeach; ?>
-                    </ul>
-                    <div class="d-flex align-items-center mt-3">
-                        <a href="details.php?id=<?php echo $index + 1; ?>" class="btn btn-orange">รายละเอียด</a>
-                        <span class="timestamp"> <?php echo $timestamp; ?></span>
-                    </div>
+                <h5 class="card-title">หัวข้อวิทยานิพนธ์: <?php echo $thesis["title"]; ?></h5>
+                <h6 class="card-subtitle">(<?php echo $thesis["eng_title"]; ?>)</h6>
+                <ul class="list-group">
+                    <?php foreach ($thesis["students"] as $student): ?>
+                        <li class="list-group-item">
+                            <strong>ชื่อ:</strong> <?php echo $student["name"]; ?> &nbsp;
+                            <strong>รหัสนิสิต:</strong> <?php echo $student["id"]; ?> &nbsp;
+                            <strong>สาขาวิชา:</strong> <?php echo $student["major"]; ?>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+                <div class="d-flex align-items-center mt-3">
+                    <a href="details.php?id=<?php echo $index + 1; ?>" class="btn-orange">รายละเอียด</a>
+                    <span class="timestamp"> <?php echo $timestamp; ?></span>
                 </div>
             </div>
         <?php endforeach; ?>
