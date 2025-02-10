f6cdf2defb863b1436327d6213a5cf26ec099a34
Anadyst <potter17180@gmail.com>
2025-02-10 21:30:45 +0700

    bugfix chat read and bugfix cant watch advisor profile in inbox

--- chat/load_messages.php
+++ chat/load_messages.php
@@ -7,6 +7,10 @@ require('../server.php');
 $sender_id = $_SESSION['id'];
 $receiver_id = $_SESSION['receiver_id'];
 $title = $_SESSION['title'];
+
+$sql = "UPDATE messages SET is_read = 1 WHERE receiver_id = '$sender_id' AND sender_id = '$receiver_id' AND is_read = 0 AND title = '$title'";
+$result = $conn->query($sql);
+
 $sql = "SELECT * FROM messages WHERE (receiver_id = '$receiver_id' AND sender_id = '$sender_id' AND title = '$title')
         OR (receiver_id = '$sender_id' AND sender_id = '$receiver_id' AND title = '$title')
         ORDER BY time_stamp ASC";
