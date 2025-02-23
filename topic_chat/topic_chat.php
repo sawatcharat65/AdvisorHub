c70927d3478514a153d6e44b8840ba955e930b75
Tanaphat Partoom <76895893+ShadowsDuck@users.noreply.github.com>
2025-02-23 13:15:51 +0700

    Update favicon from JPG to PNG format in topic chat and create pages

--- topic_chat/topic_chat.php
+++ topic_chat/topic_chat.php
@@ -126,7 +126,7 @@ if (isset($_POST['delete'])) {
     <link rel="stylesheet" href="topic_chat.css">
     <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="icon" href="../Logo.jpg">
+    <link rel="icon" href="../Logo.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 
